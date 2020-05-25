b710e9b5c6010b5adf4162152ca557032a75dcc0
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-05-25 12:12:37 +0200

    Non working attempt at plotting covariate effects

--- covariate-size-effects.r
+++ covariate-size-effects.r
@@ -1,84 +1,121 @@
 library(ggplot2)
 
-args <- commandArgs(trailingOnly = TRUE)
-filename <- args[1]
+select_covariate_effects <- function(){
+  args <- commandArgs(trailingOnly = TRUE)
+  filename <- args[1]
 
-load(paste0("results/", filename))
-filename = gsub("(.*)stanfit.Rdata$", "\\1", filename)
-out <- rstan::extract(fit)
+  load(paste0("results/", filename))
+  filename = gsub("(.*)stanfit.Rdata$", "\\1", filename)
+  out <- rstan::extract(fit)
+  if (dim(out$alpha)[2] == 6){
+    prepped_data = prepare_intervention_effects(out)
+  } else {
+    prepped_data = prepare_mobility_effects(out, formula_pooling, formula_partialpooling)
+  }
+  plot_covariate_effects(prepped_data[["data"]], prepped_data[["plot_labels"]], filename)
+}
 
-alpha = data.frame(as.matrix(out$alpha))
-plot_labels <- c("School Closure",
-                 "Self Isolation",
-                 "Public Events",
-                 "First Intervention",
-                 "Lockdown", 'Social distancing \n encouraged')
-colnames(alpha) = plot_labels
-first.intervention = alpha[,c(1,2,3,5,6)] + alpha[,4]
-data1 = bayesplot::mcmc_intervals_data(first.intervention,prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
-data1$type = "First Intervention"
+prepare_intervention_effects <- function (out){
 
-data2 = bayesplot::mcmc_intervals_data(alpha[,c(1,2,3,5,6)],  prob = .95,transformation=function(x) 1-exp(-x),point_est="mean")
-data2$type = "Later Intervention"
-# data = rbind(rbind(data2[6,],data1),data2[1:5,])
-data = rbind(data1,data2[1:5,])
-matrixStats::colQuantiles(data.matrix(first.intervention),probs=c(.025,.975))
+  alpha = data.frame(as.matrix(out$alpha))
+  plot_labels <- c("School Closure",
+                   "Self Isolation",
+                   "Public Events",
+                   "First Intervention",
+                   "Lockdown", 'Social distancing \n encouraged')
+
+  colnames(alpha) = plot_labels
+  first.intervention = alpha[,c(1,2,3,5,6)] + alpha[,4]
+  data1 = bayesplot::mcmc_intervals_data(first.intervention,prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
+  data1$type = "First Intervention"
+
+  data2 = bayesplot::mcmc_intervals_data(alpha[,c(1,2,3,5,6)],  prob = .95,transformation=function(x) 1-exp(-x),point_est="mean")
+  data2$type = "Later Intervention"
+  data = rbind(data1,data2[1:5,])
+  return (c("data"=data, "plot_labels"=plot_labels))
+}
+
+prepare_mobility_effects <- function (out, formula_pooling, formula_partialpooling) {
+  
+  stop("covariate effect plot not supported for mobility")
+  plot_labels = regmatches(formula_pooling, gregexpr("[a-zA-Z]+", formula_pooling))[[1]]
+  plot_labels_partial = regmatches(formula_partialpooling, gregexpr("[a-zA-Z]+", formula_partialpooling))[[1]]
+  
+  alpha = data.frame(as.matrix(out$alpha))
+  # alpha_partial = data.frame(as.matrix(out$alpha_state))
+
+  colnames(alpha) = plot_labels
+  # colnames(alpha_partial) = plot_labels_partial
+  data1 = bayesplot::mcmc_intervals_data(alpha,prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
+  data1$type = "pooled"
+  # data2 = bayesplot::mcmc_intervals_data(alpha_partial[,],prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
+  # data2$type = "partial"
+  # data = rbind(data1,data2)
+  return (c("data"=data1, "plot_labels"=plot_labels))
+
+}
 
 #data$type[1] = "First Intervention"
+plot_covariate_effects <- function (data, plot_labels, filename) {
 
-levels(data$parameter) = gsub("t(", "", levels(data$parameter), fixed=TRUE)
-levels(data$parameter) = gsub(")", "", levels(data$parameter), fixed=TRUE)
-data$parameter = (as.character(data$parameter))
+  levels(data$parameter) = gsub("t(", "", levels(data$parameter), fixed=TRUE)
+  levels(data$parameter) = gsub(")", "", levels(data$parameter), fixed=TRUE)
+  data$parameter = (as.character(data$parameter))
 
-no_point_est <- all(data$point_est == "none")
-x_lim <- range(c(data$ll, data$hh))
-x_range <- diff(x_lim)
-x_lim[1] <- x_lim[1] - 0.05 * x_range
-x_lim[2] <- x_lim[2] + 0.05 * x_range
-layer_vertical_line <- if (0 > x_lim[1] && 0 < x_lim[2]) {
-  bayesplot::vline_0(color = "gray90", size = 0.5)
-} else {
-  geom_blank(
-    mapping = NULL, data = NULL,
-    show.legend = FALSE, inherit.aes = FALSE)
-}
-args_outer <- list(mapping = aes_(x = ~ll, xend = ~hh, y = ~parameter, 
-                                  yend = ~parameter)) #, color = bayesplot::get_color("mid"))
-args_inner <- list(mapping = aes_(x = ~l, xend = ~h, y = ~parameter, 
-                                  yend = ~parameter), size = 2, show.legend = FALSE)
-args_point <- list(mapping = aes_(x = ~m, y = ~parameter), 
-                   data = data, size = 4, shape = 21)
+  no_point_est <- all(data$point_est == "none")
+  x_lim <- range(c(data$ll, data$hh))
+  x_range <- diff(x_lim)
+  x_lim[1] <- x_lim[1] - 0.05 * x_range
+  x_lim[2] <- x_lim[2] + 0.05 * x_range
+  layer_vertical_line <- if (0 > x_lim[1] && 0 < x_lim[2]) {
+    bayesplot::vline_0(color = "gray90", size = 0.5)
+  } else {
+    geom_blank(
+      mapping = NULL, data = NULL,
+      show.legend = FALSE, inherit.aes = FALSE)
+  }
+  args_outer <- list(mapping = aes_(x = ~ll, xend = ~hh, y = ~parameter, 
+                                    yend = ~parameter)) #, color = bayesplot::get_color("mid"))
+  args_inner <- list(mapping = aes_(x = ~l, xend = ~h, y = ~parameter, 
+                                    yend = ~parameter), size = 2, show.legend = FALSE)
+  args_point <- list(mapping = aes_(x = ~m, y = ~parameter), 
+                     data = data, size = 4, shape = 21)
 
-args_point$color <- "blue" #get_color("dark_highlight")
+  args_point$color <- "blue" #get_color("dark_highlight")
 
-point_func <- geom_point
-layer_outer <- do.call(geom_segment, args_outer)
-layer_inner <- do.call(geom_segment, args_inner)
-layer_point <- do.call(point_func, args_point)
+  point_func <- geom_point
+  layer_outer <- do.call(geom_segment, args_outer)
+  layer_inner <- do.call(geom_segment, args_inner)
+  layer_point <- do.call(point_func, args_point)
 
-data$parameter = factor(as.character(data$parameter),levels=plot_labels[order(plot_labels)[6:1]])
-# data = data[order(-data$m),]
-p = ggplot(data) +ggpubr::theme_pubr() +  geom_point(aes(x=m,y=parameter,colour=type),position = position_dodge(-.5)) + 
-  geom_linerange(aes(xmin=ll,xmax=hh,y=parameter,colour=type),
-                 position = position_dodge(-.5)) + 
-  scale_x_continuous(breaks=seq(0,1,.25),labels = c("0%\n(no effect on transmissibility)",
-                                                    "25%","50%","75%","100%\n(ends transmissibility)"),
-                     expand=c(0.005,0.005),expression(paste("Relative % reduction in  ",R[t])))  +
-  scale_colour_manual(name = "", #labels = c("50%", "95%"),
-                      values = c(("coral4"), ("seagreen"))) + 
+  data$parameter = factor(as.character(data$parameter),levels=plot_labels[order(plot_labels)[length(plot_labels):1]])
+  # data = data[order(-data$m),]
+  p = ggplot(data) + ggpubr::theme_pubr() +  geom_point(
+      aes(x=m,y=parameter,colour=type),position = position_dodge(-.5)
+    ) + geom_linerange(
+      aes(xmin=ll,xmax=hh,y=parameter,colour=type), position = position_dodge(-.5)
+    ) + 
+    scale_x_continuous(
+      breaks=seq(0,1,.25), labels = c(
+        "0%\n(no effect on transmissibility)","25%","50%","75%","100%\n(ends transmissibility)"
+      ), expand=c(0.005,0.005),expression(paste("Relative % reduction in  ",R[t]))
+    ) + scale_colour_manual(name = "", values = c(("coral4"), ("seagreen")))
+    + geom_vline(xintercept=1,colour="darkgray")
+    + scale_y_discrete("Governmental intervention\n")
+    #geom_vline(xintercept=0,colour="darkgray") + 
+    + theme(plot.margin = margin(0, 2, 0, .5, "cm"))
+  #+ guides(fill=guide_legend(nrow=2))
+
+  ggsave(filename = paste0("results/", filename, "covars-alpha-reduction.png"),
+         p,height=4,width=8)
+  write.csv(data, paste0("results/", filename, "covars-alpha-reduction.csv"))
+  dir.create("web/figures/desktop/", showWarnings = FALSE, recursive = TRUE)
+  dir.create("web/figures/mobile/", showWarnings = FALSE, recursive = TRUE)
+  cowplot::save_plot(filename = paste0("web/figures/desktop/",  "covars-alpha-reduction.svg"), 
+            p, base_height = 4, base_asp = 1.618 * 2 * 8/12)
+  cowplot::save_plot(filename = paste0("web/figures/mobile/", "covars-alpha-reduction.svg"), 
+            p, base_height = 4, base_asp = 1.1)
   
-  geom_vline(xintercept=1,colour="darkgray") +
-  scale_y_discrete("Governmental intervention\n") +
-  #geom_vline(xintercept=0,colour="darkgray") + 
-  theme(plot.margin = margin(0, 2, 0, .5, "cm"))
-#+ guides(fill=guide_legend(nrow=2))
+}
 
-ggsave(filename = paste0("results/", filename, "covars-alpha-reduction.png"),
-       p,height=4,width=8)
-write.csv(data, paste0("results/", filename, "covars-alpha-reduction.csv"))
-dir.create("web/figures/desktop/", showWarnings = FALSE, recursive = TRUE)
-dir.create("web/figures/mobile/", showWarnings = FALSE, recursive = TRUE)
-cowplot::save_plot(filename = paste0("web/figures/desktop/",  "covars-alpha-reduction.svg"), 
-          p, base_height = 4, base_asp = 1.618 * 2 * 8/12)
-cowplot::save_plot(filename = paste0("web/figures/mobile/", "covars-alpha-reduction.svg"), 
-          p, base_height = 4, base_asp = 1.1)
+select_covariate_effects()
