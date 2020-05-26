9db9fe0f570105ab45aafb645b0cb6daf62badad
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-05-26 18:05:31 +0200

    Correctly load GCMR data

--- data/update-french-regional-data.sh
+++ data/update-french-regional-data.sh
@@ -6,7 +6,7 @@ datadir="data/FRA/"
 source_opencovid="https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.csv"
 source_INSEE_reg="https://raw.githubusercontent.com/scrouzet/covid19-incrementality/master/data/departements.csv"
 source_INSEE_dep="https://raw.githubusercontent.com/scrouzet/covid19-incrementality/master/data/INSEE%20-%20year%20x%20dept%20x%20sex%20x%20age%20-%20population.csv"
-
+source_GCMR="https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv"
 # EHPAD source http://www.data.drees.sante.gouv.fr/ReportFolders/reportFolders.aspx
 
 wget -O ${datadir}/opencovid19-fr-chiffres-cles.csv ${source_opencovid}
@@ -14,7 +14,7 @@ wget -O ${datadir}/population-fra-INSEE-region-departement.csv ${source_INSEE_re
 wget -O ${datadir}/population-fra-INSEE-departement.csv ${source_INSEE_dep}
 
 # Get google mobility data
-wget https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv
+wget -O data/Global_Mobility_Report.csv ${source_GCMR}
 
 python3 data/extract_opencovidfr_2_ICL.py data/FRA/opencovid19-fr-chiffres-cles.csv all-france
 python3 data/extract_opencovidfr_2_ICL.py data/FRA/opencovid19-fr-chiffres-cles.csv REG
