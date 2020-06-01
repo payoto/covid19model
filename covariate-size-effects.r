475bdb6ef6ae391aacab8d2f17b7d1e6431289a2
Alexandre Payot <alexandre.payotli@gmail.com>
2020-06-02 00:18:07 +0200

    Revamp the covariate effect calculation
    This commit adds support for covariate effect calculation including
    partially pooled ones with the mobility data.

--- covariate-size-effects.r
+++ covariate-size-effects.r
@@ -3,55 +3,66 @@ library(ggplot2)
 select_covariate_effects <- function(){
   args <- commandArgs(trailingOnly = TRUE)
   filename <- args[1]
-
+  VERSION = "v2" # Last version with no VERSION variable definition (overwritten by load)
   load(paste0("results/", filename))
   filename = gsub("(.*)stanfit.Rdata$", "\\1", filename)
   out <- rstan::extract(fit)
-  if (dim(out$alpha)[2] == 6){
+  if (VERSION == "v3" || VERSION == "v2"){
     prepped_data = prepare_intervention_effects(out)
   } else {
-    prepped_data = prepare_mobility_effects(out, formula_pooling, formula_partialpooling)
+    prepped_data = prepare_mobility_effects(out, formula_pooling, formula_partialpooling, region_to_country_map)
   }
   plot_covariate_effects(prepped_data[["data"]], prepped_data[["plot_labels"]], filename)
 }
 
 prepare_intervention_effects <- function (out){
 
-  alpha = data.frame(as.matrix(out$alpha))
+  alpha <- data.frame(as.matrix(out$alpha))
   plot_labels <- c("School Closure",
                    "Self Isolation",
                    "Public Events",
                    "First Intervention",
                    "Lockdown", 'Social distancing \n encouraged')
 
-  colnames(alpha) = plot_labels
-  first.intervention = alpha[,c(1,2,3,5,6)] + alpha[,4]
-  data1 = bayesplot::mcmc_intervals_data(first.intervention,prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
-  data1$type = "First Intervention"
+  colnames(alpha) <- plot_labels
+  first.intervention <- alpha[,c(1,2,3,5,6)] + alpha[,4]
+  data1 <- bayesplot::mcmc_intervals_data(first.intervention,prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
+  data1$type <- "First Intervention"
 
-  data2 = bayesplot::mcmc_intervals_data(alpha[,c(1,2,3,5,6)],  prob = .95,transformation=function(x) 1-exp(-x),point_est="mean")
-  data2$type = "Later Intervention"
-  data = rbind(data1,data2[1:5,])
-  return (c("data"=data, "plot_labels"=plot_labels))
+  data2 <- bayesplot::mcmc_intervals_data(alpha[,c(1,2,3,5,6)],  prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
+  data2$type <- "Later Intervention"
+  data <- rbind(data1,data2[1:5,])
+  return(list(data=data, plot_labels=plot_labels))
 }
 
-prepare_mobility_effects <- function (out, formula_pooling, formula_partialpooling) {
+prepare_mobility_effects <- function (out, formula_pooling, formula_partialpooling, region_to_country_map) {
   
-  stop("covariate effect plot not supported for mobility")
-  plot_labels = regmatches(formula_pooling, gregexpr("[a-zA-Z]+", formula_pooling))[[1]]
-  plot_labels_partial = regmatches(formula_partialpooling, gregexpr("[a-zA-Z]+", formula_partialpooling))[[1]]
-  
-  alpha = data.frame(as.matrix(out$alpha))
-  # alpha_partial = data.frame(as.matrix(out$alpha_state))
+  # stop("covariate effect plot not supported for mobility")
 
-  colnames(alpha) = plot_labels
-  # colnames(alpha_partial) = plot_labels_partial
-  data1 = bayesplot::mcmc_intervals_data(alpha,prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
-  data1$type = "pooled"
-  # data2 = bayesplot::mcmc_intervals_data(alpha_partial[,],prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
-  # data2$type = "partial"
-  # data = rbind(data1,data2)
-  return (c("data"=data1, "plot_labels"=plot_labels))
+  plot_labels <- regmatches(formula_pooling, gregexpr("[a-zA-Z]+", formula_pooling))[[1]]
+  name_labels_partial <- regmatches(formula_partialpooling, gregexpr("[a-zA-Z]+", formula_partialpooling))[[1]]
+  
+  alpha <- data.frame(as.matrix(out$alpha))
+  plot_labels <- paste("global: ",plot_labels,sep="")
+  colnames(alpha) <- plot_labels
+  data_global <- bayesplot::mcmc_intervals_data(alpha,prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
+  data_global$type <- "pooled"
+
+  plot_labels_partial <- c()
+  for(i in 1:length(region_to_country_map)){
+    zone <- names(region_to_country_map)[i]
+    zone_labels <- paste(paste0(zone, ": "), name_labels_partial, sep="")
+    plot_labels_partial <- c(plot_labels_partial, zone_labels)
+    alpha_partial <- data.frame(as.matrix(drop(out$alpha_state[,i,])))
+    colnames(alpha_partial) <- zone_labels
+    data_partial <- bayesplot::mcmc_intervals_data(alpha_partial,prob=.95,transformation=function(x) 1-exp(-x),point_est="mean")
+    data_partial$type <- "partial"
+    data <- rbind(data_global,data_partial)
+  }
+
+  plot_labels <- c(plot_labels, plot_labels_partial)
+  print(plot_labels)  
+  return (list(data=data, plot_labels=plot_labels))
 
 }
 
@@ -99,11 +110,11 @@ plot_covariate_effects <- function (data, plot_labels, filename) {
       breaks=seq(0,1,.25), labels = c(
         "0%\n(no effect on transmissibility)","25%","50%","75%","100%\n(ends transmissibility)"
       ), expand=c(0.005,0.005),expression(paste("Relative % reduction in  ",R[t]))
-    ) + scale_colour_manual(name = "", values = c(("coral4"), ("seagreen")))
-    + geom_vline(xintercept=1,colour="darkgray")
-    + scale_y_discrete("Governmental intervention\n")
+    ) + scale_colour_manual(name = "", values = c(("coral4"), ("seagreen"))) +
+    geom_vline(xintercept=1,colour="darkgray") +
+    scale_y_discrete("Governmental intervention\n") +
     #geom_vline(xintercept=0,colour="darkgray") + 
-    + theme(plot.margin = margin(0, 2, 0, .5, "cm"))
+    theme(plot.margin = margin(0, 2, 0, .5, "cm"))
   #+ guides(fill=guide_legend(nrow=2))
 
   ggsave(filename = paste0("results/", filename, "covars-alpha-reduction.png"),
@@ -118,4 +129,4 @@ plot_covariate_effects <- function (data, plot_labels, filename) {
   
 }
 
-select_covariate_effects()
+# select_covariate_effects()
